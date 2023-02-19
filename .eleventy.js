b90d121a34f2d2ac0ce7211948daa35c81ac7147
kenokenobingo <mail.kewe@gmail.com>
2023-02-19 12:20:23 +0100

    Formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,58 +1,57 @@
-const fs = require('fs');
-const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
+const fs = require("fs");
+const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 
-module.exports = function(eleventyConfig) {
-    let markdownIt = require('markdown-it');
-    let markdownItFootnote = require("markdown-it-footnote");
-    let options = {
-      html: true
-    };
-
-    eleventyConfig.addShortcode("figure", require("./utils/figure.js"));
-
-    eleventyConfig.addPassthroughCopy('img');
-    eleventyConfig.addPassthroughCopy('css');
-
-    eleventyConfig.addPlugin(lazyImagesPlugin, {
-      imgSelector: 'img'  
-    });
-
-    let markdownLib = markdownIt(options).use(markdownItFootnote);
-    eleventyConfig.setLibrary("md", markdownLib);
-  
-    eleventyConfig.setBrowserSyncConfig({
-      callbacks: {
-        ready: function(err, browserSync) {
-          const content_404 = fs.readFileSync('_site/404/index.html');
-  
-          browserSync.addMiddleware('*', (req, res) => {
-            // Provides the 404 content without redirect.
-            res.write(content_404);
-            res.end();
-          });
-        },
-      },
-    });
-  
-    return {
-      templateFormats: ['md', 'njk', 'html', 'liquid'],
-  
-      // If your site lives in a different subdirectory, change this.
-      // Leading or trailing slashes are all normalized away, so don’t worry about it.
-      // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-      // This is only used for URLs (it does not affect your file structure)
-      pathPrefix: '/',
-  
-      markdownTemplateEngine: 'liquid',
-      htmlTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-      passthroughFileCopy: true,
-      dir: {
-        input: '.',
-        includes: '_includes',
-        data: '_data',
-        output: '_site',
-      },
-    };
+module.exports = function (eleventyConfig) {
+  let markdownIt = require("markdown-it");
+  let markdownItFootnote = require("markdown-it-footnote");
+  let options = {
+    html: true,
   };
 
+  eleventyConfig.addShortcode("figure", require("./utils/figure.js"));
+
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+
+  eleventyConfig.addPlugin(lazyImagesPlugin, {
+    imgSelector: "img",
+  });
+
+  let markdownLib = markdownIt(options).use(markdownItFootnote);
+  eleventyConfig.setLibrary("md", markdownLib);
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        const content_404 = fs.readFileSync("_site/404/index.html");
+
+        browserSync.addMiddleware("*", (req, res) => {
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      },
+    },
+  });
+
+  return {
+    templateFormats: ["md", "njk", "html", "liquid"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+  };
+};
